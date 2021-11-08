affdeafa10ccd9dedef6941135b0799f6f8d007f
Eamon <eamon0989@gmail.com>
2021-11-08 13:18:01 +0100

    added useEffect in phonebook

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Search } from './components/search'
 import { Form } from './components/form'
 import { Persons } from './components/persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }
+
+  useEffect(hook, [])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')

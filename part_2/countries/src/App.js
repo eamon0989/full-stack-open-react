f070d6f96137f3410ca376a1ba7249d02d3c6038
Eamon <eamon0989@gmail.com>
2021-11-08 17:11:49 +0100

    get weather api working

--- part_2/countries/src/App.js
+++ part_2/countries/src/App.js
@@ -1,34 +1,65 @@
 import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+const api_key = process.env.REACT_APP_API_KEY
 
-const Country = ({ name }) => {
+const Country = ({ country, showCountry }) => {
   return (
     <>
-      <li>{name}</li>
+      <li>{country.name.common}<button onClick={showCountry}> show</button></li>
     </>
   )
 }
 
-const SingleCountry = ({ country }) => {
-  const displayCountry = country[0];
+const SingleCountry = (props) => {
+  const displayCountry = props.country[0];
+  const [ weather, setWeather ] = useState({})
 
-  return (
-    <li>
-      <h1>{displayCountry.name.common}</h1>
-      <p>capital: {displayCountry.capital[0]}</p>
-      <p>population: {displayCountry.population}</p>
-      <h2>languages</h2>
-      <ul>
-        {Object.values(displayCountry.languages).map(lang => <li key={lang}>{lang}</li>)}
-      </ul>
+  const hook = () => {
+    const capital = displayCountry.capital;
 
-      <img src={displayCountry.flags.png} alt='flag' />
-    </li>
-  )
+    axios
+    .get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${capital}`)
+    .then(res => setWeather(res.data))
+  }
+
+  useEffect(hook, [])
+
+  if (weather?.current) {
+    return (
+      <li>
+        <h1>{displayCountry.name.common}</h1>
+        <p>capital: {displayCountry.capital[0]}</p>
+        <p>population: {displayCountry.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {Object.values(displayCountry.languages).map(lang => <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={displayCountry.flags.png} alt='flag' />
+
+        <h3>Weather in {weather.location.name}</h3>
+        <p><b>temperature:</b> {weather.current.temp_c}</p>
+        <img src={weather.current.condition.icon} alt='weather' />
+        <p><b>wind:</b> {weather.current.wind_kph} kph direction {weather.current.wind_dir}</p>
+      </li>
+    )
+  } else {
+    return (
+      <li>
+        <h1>{displayCountry.name.common}</h1>
+        <p>capital: {displayCountry.capital[0]}</p>
+        <p>population: {displayCountry.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {Object.values(displayCountry.languages).map(lang => <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={displayCountry.flags.png} alt='flag' />
+      </li>
+    )
+  }
 }
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, showCountry }) => {
   if (countries.length > 10) {
     return (
       <ul>
@@ -44,7 +75,7 @@ const Countries = ({ countries }) => {
   } else {
     return (
       <ul>
-        {countries.map(country => <Country key={country.name.common} name={country.name.common} /> )}
+        {countries.map(country => <Country key={country.name.common} country={country} showCountry={showCountry} /> )}
       </ul>
     )
   }
@@ -83,12 +114,13 @@ function App() {
   }
 
   useEffect(hook, [])
+
   return (
     <>
       <Search updateSearch={updateSearch} />
       <Countries countries={countriesToShow} />
     </>
-  );
+  )
 }
 
 export default App;

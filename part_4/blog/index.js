707341e703213119d003750fe03a3fe6d394abda
Eamon <eamon0989@gmail.com>
2021-11-15 13:24:17 +0100

    added controllers and model

--- part_4/blog/index.js
+++ part_4/blog/index.js
@@ -3,43 +3,20 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = process.env.MONGODB_URI
 
 mongoose.connect(mongoUrl)
+  .then(() => console.log(`Connected to Mongo`))
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
